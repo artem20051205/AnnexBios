7e1ed70b1765316808499b17e1d183ea2ff27fcd
ivanshishkov-debug <ivanshishkov.ua@gmail.com>
2026-09-18 14:05:50 +0200

    db databases

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
+<?php
+
+require_once 'db.php';
+
+$stmt = $pdo->query("SELECT * FROM movies");
+
+$movies = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($movies as $movie) {
+    echo $movie['title'] . '<br>';
+} ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
+    <?php 
+    echo "SELECT * FROM movies WHERE title = 'Dune'";
+    
+    
+    
+    ?>
 <header>
     <a href="index.html">
         <img src="style/images/logo.png" alt="Logo" width="300" class="logo">
@@ -26,6 +44,7 @@
 <p1>Ontdek de laatste films in comfort</p1>
 <button>Film Agenda Bekijken</button>
 
+
 <footer>
     <p>© 2026 AnnexBios Bilthoven</p>
 
@@ -35,5 +54,7 @@
         <a href="bestellen.html">Bestellen</a>
     </div>
 </footer>
+
 </body>
+
 </html>
