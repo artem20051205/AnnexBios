ab1526a7cb1d4b51b3d2e78b06803e2ad5cee92f
ExoticYolo123 <164374524+ExoticYolo123@users.noreply.github.com>
2026-09-25 10:24:29 +0200

    tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.php",
+    "./includes/**/*.php",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
