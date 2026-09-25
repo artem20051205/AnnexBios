638c95b63bbabaf64a077e658b92ebc1c32f9767
lucasa030 <250579@student.glu.nl>
2026-09-25 11:20:00 +0200

    header tailwind

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,33 @@
-<header>
-        <a href="index.php">
-            <img src="style/images/logo.png" alt="Logo" width="300" class="logo">
+<header class="flex items-center rounded-lg bg-[rgb(20,23,25)] px-6 py-3">
+
+    <a href="index.php">
+        <img
+            src="style/images/logo.png"
+            alt="Logo"
+            class="w-[180px]"
+        >
+    </a>
+
+    <nav class="ml-12 flex items-center gap-8">
+        <a href="index.php" class="text-lg text-white hover:text-[#67294c]">
+            Home
         </a>
 
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="films.php">Filmagenda</a>
-            <a href="film-detail.php">Film detail</a>
-            <a href="bestellen.php">Bestellen</a>
-            <a href="admin.html">Admin</a>
-        </nav>
-    </header>
+        <a href="films.php" class="text-lg text-white hover:text-[#67294c]">
+            Filmagenda
+        </a>
+
+        <a href="film-detail.php" class="text-lg text-white hover:text-[#67294c]">
+            Film detail
+        </a>
+
+        <a href="bestellen.php" class="text-lg text-white hover:text-[#67294c]">
+            Bestellen
+        </a>
+
+        <a href="admin.html" class="text-lg text-white hover:text-[#67294c]">
+            Admin
+        </a>
+    </nav>
+
+</header>
