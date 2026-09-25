0e4a5ec323b467582408e6ad87f9d9e4700eaf72
ExoticYolo123 <164374524+ExoticYolo123@users.noreply.github.com>
2026-09-25 14:52:21 +0200

    new design

--- film-detail.php
+++ film-detail.php
@@ -6,43 +6,117 @@ $id = (int) ($_GET['id'] ?? 0);
 
 $stmt = $pdo->prepare("SELECT * FROM movies WHERE movie_id = ?");
 $stmt->execute([$id]);
-$movie = $stmt->fetch(PDO::FETCH_ASSOC); // одна строка или false
+$movie = $stmt->fetch(PDO::FETCH_ASSOC);
 
 $voorstellingen = [];
+
 if ($movie) {
-    $stmt = $pdo->prepare("SELECT * FROM voorstelling WHERE film_id = ? ORDER BY datum, begintijd");
+    $stmt = $pdo->prepare("
+        SELECT * 
+        FROM voorstelling 
+        WHERE film_id = ? 
+        ORDER BY datum, begintijd
+    ");
+
     $stmt->execute([$id]);
+
     $voorstellingen = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
 <?php include 'includes/head.php'; ?>
 
 <body>
+
     <?php include 'includes/header.php'; ?>
 
-    <?php if ($movie): ?>
-        <h1><?= htmlspecialchars($movie['title']) ?></h1>
-        <p><?= htmlspecialchars($movie['description'] ?? '') ?></p>
-        <p>release date: <?= htmlspecialchars($movie['release_date'] ?? '') ?></p>
-        <p>Raiting: <?= htmlspecialchars($movie['imd_rating'] ?? '') ?></p>
-        <poster>
-            <img src="<?= htmlspecialchars($movie['poster'] ?? 'style/images/Placeholder.png') ?>" alt="Poster" width="200">
-        </poster>
 
-        <h2>Voorstellingen</h2>
-        <?php foreach ($voorstellingen as $v): ?>
-            <a href="bestellen.php?voorstelling=<?= $v['voorstelling_id'] ?>">
-                <?= htmlspecialchars($v['datum'] . ' ' . $v['begintijd']) ?>
-            </a>
-        <?php endforeach; ?>
+    <?php if ($movie): ?>
+
+        <div class="detail-container">
+
+            <div class="detail-movie">
+
+                <img
+                    class="detail-poster"
+                    src="<?= htmlspecialchars($movie['poster'] ?? 'style/images/Placeholder.png') ?>"
+                    alt="Poster"
+                >
+
+
+                <div class="detail-info">
+
+                    <h1>
+                        <?= htmlspecialchars($movie['title']) ?>
+                    </h1>
+
+
+                    <p class="detail-description">
+                        <?= htmlspecialchars($movie['description'] ?? '') ?>
+                    </p>
+
+
+                    <p>
+                        <strong>Release date:</strong>
+                        <?= htmlspecialchars($movie['release_date'] ?? '') ?>
+                    </p>
+
+
+                    <p>
+                        <strong>Rating:</strong>
+                        <?= htmlspecialchars($movie['imd_rating'] ?? '') ?>
+                    </p>
+
+
+                    <h2>
+                        Voorstellingen
+                    </h2>
+
+
+                    <div class="showtimes">
+
+                        <?php foreach ($voorstellingen as $v): ?>
+
+                            <a href="bestellen.php?voorstelling=<?= $v['voorstelling_id'] ?>">
+
+                                <?= htmlspecialchars($v['datum']) ?>
+
+                                <br>
+
+                                <?= htmlspecialchars($v['begintijd']) ?>
+
+                            </a>
+
+                        <?php endforeach; ?>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
     <?php else: ?>
-        <h1>Unknown Film</h1>
+
+        <div class="detail-container">
+
+            <h1>
+                Unknown Film
+            </h1>
+
+        </div>
+
     <?php endif; ?>
 
+
     <?php include 'includes/footer.php'; ?>
+
 </body>
 
 </html>
